676a5f36de3f9503f9c37a8f312516b2c2d66ced
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-08-04 12:36:53 +0330

    use get method

---
+++ data/todos.js
@@ -0,0 +1,8 @@
+export const todos =[
+    {id:1 , title : "todod 1"},
+    {id:2 , title : "todod 2"},
+    {id:3 , title : "todod 3"},
+    {id:4 , title : "todod 4"},
+    {id:5 , title : "todod 5"},
+
+];
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
 
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const[todos , setTodos]=useState([]);
+
+  useEffect(()=>{
+    async function fetchData(){
+        const res = await fetch("/api/todos");
+        const data = await res.json();
+
+        setTodos(data);
+        console.log(data)
+    }
+    fetchData()
+  },[])
   return (
     <div className={styles.container}>
      <h1>To Do's Page</h1>
+     <ul>
+      {
+        todos.map((todo) => (<li key={todo.id}>{todo.title}</li>))
+      }
+     </ul>
+
     </div>
   )
 }
