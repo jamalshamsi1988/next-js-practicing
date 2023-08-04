3f2e20faba6f1587004146a4490daa80043dbc9a
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-08-04 12:11:16 +0330

    create next app

---
+++ data/todods.js
@@ -0,0 +1,8 @@
+const tdods =[
+    {id:1 , todod : "title 1"},
+    {id:2 , todod : "title 2"},
+    {id:3 , todod : "title 3"},
+    {id:4 , todod : "title 4"},
+    {id:5 , todod : "title 5"},
+
+];
