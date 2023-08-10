ff3fe1a9327ed5df38f7791c176e6b86225e6295
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-08-10 11:26:50 +0330

    how use getStaticProps for clint API

---
+++ pages/todos.js
@@ -0,0 +1,20 @@
+import { todos } from "../data/todos";
+
+const Todos = ({data}) => {
+  return (
+    <div>
+      {
+        data.map((todo )=> (<h1 key={todo.id}>{todo.title}</h1>))
+      }
+    </div>
+  )
+}
+
+export default Todos;
+
+export async function getStaticProps(){
+
+    return {
+        props :{ data : todos},
+    }
+}
